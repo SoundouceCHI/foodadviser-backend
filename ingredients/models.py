a3ff7276a4ab3583fa809a7310a44d2193f5848a
unknown <soundouce.chibani@gmail.com>
2024-12-17 20:07:06 +0100

    create Ingredient model and generate migrations

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model): 
+    name = models.CharField(max_length=55, unique=True, verbose_name="name")
+    image_url = models.URLField(max_length=500, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
