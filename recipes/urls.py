b0281b4762bcd99a94533c437a6ceaaf02295b50
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2024-12-17 20:54:42 +0100

    fetch recipe list and recipe detail

---
+++ recipes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import get_recipe, get_recipes_list
+
+urlpatterns = [
+    path('<int:recipe_id>/', get_recipe, name='get_recipe'),  
+    path('list/', get_recipes_list, name='get_recipes_list'), 
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+API_KEY = ''
+
+def get_recipe(request, recipe_id):
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}&includeNutrition=true'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({"error": "Unable to fetch recipe data"}, status=500)
+
+
+def get_recipes_list(request):
+    number = request.GET.get('number', 100)  
+
+    # url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&query={query}&cuisine={cuisine}&diet={diet}&number={number}'
+    url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&number={number}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({"error": "Unable to fetch recipes list"}, status=500)
