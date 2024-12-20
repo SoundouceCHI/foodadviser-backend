a767de4d9ecb2c7448ecd8f2b1f4f39ec939412a
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2024-12-20 19:38:02 +0100

    Get the list of recipes and their details

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_recipe, get_recipes_list
 
 urlpatterns = [
-    path('<int:recipe_id>/', get_recipe, name='get_recipe'),  
+    path('<int:recipe_id>', get_recipe, name='get_recipe'),  
     path('list/', get_recipes_list, name='get_recipes_list'), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,15 @@
 from django.core.cache import cache
 import requests
 from django.http import JsonResponse
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-# my first account
-# API_KEY = '34bba94fff724a70b81a614c97a87016'
-# my second account
-API_KEY = '46b4b79dcc6c48b1a8d0687640f32afe'
+api_key = os.getenv('API_KEY')
 
 def get_recipe(request, recipe_id):
-    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}&includeNutrition=true'
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}&includeNutrition=true'
     response = requests.get(url)
     
     if response.status_code == 200:
@@ -22,8 +22,7 @@ def get_recipe(request, recipe_id):
 def get_recipes_list(request):
     number = request.GET.get('number', 100)  
 
-    # url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&query={query}&cuisine={cuisine}&diet={diet}&number={number}'
-    url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&number={number}'
+    url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&number={number}'
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -33,28 +32,5 @@ def get_recipes_list(request):
         return JsonResponse({"error": "Daily points limit reached. Please try again tomorrow."}, status=402)
     else:
         return JsonResponse({"error": "Unable to fetch recipes list"}, status=500)
-
-
-# def get_recipes_list(request):
-#     number = request.GET.get('number', 5)
-#     cache_key = f"recipes_list_{number}"
-#     cached_data = cache.get(cache_key)
-
-#     if cached_data:
-#         return JsonResponse(cached_data)
-
-#     url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&number={number}'
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         data = response.json()
-#         cache.set(cache_key, data, timeout=3600)  
-#         return JsonResponse(data)
-#     elif response.status_code == 402:
-#         return JsonResponse({"error": "Daily points limit reached. Please try again tomorrow."}, status=402)
-#     else:
-#         return JsonResponse({"error": "Unable to fetch recipes list"}, status=response.status_code)
-
-
 
 
