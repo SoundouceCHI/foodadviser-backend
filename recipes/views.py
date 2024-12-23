a1df01783fc50554ca48ca051f758e1914861f1a
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2024-12-23 21:43:45 +0100

    add get methods in recipes.views

--- recipes/views.py
+++ recipes/views.py
@@ -10,36 +10,58 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 def get_recipe(request, recipe_id):
-    cache_key = f"recipe_{recipe_id}"
-    cached_data = cache.get(cache_key)
+    try:
+        recipe = Recipe.objects.get(id_recipe=recipe_id)
+        nutrition = Nutrition.objects.filter(recipe=recipe).first()  
 
-    if cached_data:
-        return JsonResponse(cached_data)
-
-    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}&includeNutrition=true'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        cache.set(cache_key, data, timeout=3600)  # Cache the data for an hour
+        data = {
+            'id_recipe': recipe.id_recipe,
+            'title': recipe.title,
+            'image_url': recipe.image_url,
+            'instructions': recipe.instructions,
+            'servings': recipe.servings,
+            'ready_in_minutes': recipe.ready_in_minutes,
+            'vegetarian': recipe.vegetarian,
+            'vegan': recipe.vegan,
+            'very_popular': recipe.very_popular,
+            'preparation_minutes': recipe.preparation_minutes,
+            'cooking_minutes': recipe.cooking_minutes,
+            'health_score': recipe.health_score,
+            'steps': recipe.steps,
+            'nutrition': {
+                'calories': nutrition.calories if nutrition else None,
+                'protein': nutrition.protein if nutrition else None,
+                'fat': nutrition.fat if nutrition else None,
+                'carbohydrates': nutrition.carbohydrates if nutrition else None,
+                'sugar': nutrition.sugar if nutrition else None,
+                'fiber': nutrition.fiber if nutrition else None,
+                'sodium': nutrition.sodium if nutrition else None,
+            } if nutrition else None,
+        }
         return JsonResponse(data)
-    else:
-        return JsonResponse({"error": "Unable to fetch recipe data"}, status=500)
+    except Recipe.DoesNotExist:
+        return JsonResponse({"error": "Recipe not found"}, status=404)
 
 
 def get_recipes_list(request):
-    number = request.GET.get('number', 100)  
+    number = int(request.GET.get('number', 100))  
+    recipes = Recipe.objects.all()[:number]  
+    recipes_data = []
 
-    url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&number={number}'
-    response = requests.get(url)
+    for recipe in recipes:
+        recipes_data.append({
+            'id_recipe': recipe.id_recipe,
+            'title': recipe.title,
+            'image_url': recipe.image_url,
+            'servings': recipe.servings,
+            'ready_in_minutes': recipe.ready_in_minutes,
+            'vegetarian': recipe.vegetarian,
+            'vegan': recipe.vegan,
+            'very_popular': recipe.very_popular,
+        })
 
-    if response.status_code == 200:
-        data = response.json()
-        return JsonResponse(data)
-    elif response.status_code == 402:
-        return JsonResponse({"error": "Daily points limit reached. Please try again tomorrow."}, status=402)
-    else:
-        return JsonResponse({"error": "Unable to fetch recipes list"}, status=500)
+    return JsonResponse({'recipes': recipes_data})
+
 
     
 
