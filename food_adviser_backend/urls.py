e36f317493f10f3ce66ca9505685694582aa2e22
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2025-01-02 23:56:43 +0100

    Update URL configuration to include image manager app

--- food_adviser_backend/urls.py
+++ food_adviser_backend/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from recipes import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls')),
     path('ingredients/', include('ingredients.urls')), 
     path('get_recipes/', include('get_recipes.urls')),
+    path('image_manager/', include('image_manager.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
