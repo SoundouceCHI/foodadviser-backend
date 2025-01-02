f2cd5a2c96e81b20873159ca6dd2c34ecf481cae
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2025-01-02 23:57:56 +0100

    Enabled media file handling with appropriate MEDIA_URL and MEDIA_ROOT settings

--- food_adviser_backend/settings.py
+++ food_adviser_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'ingredients', 
     'recipes',
     'recipeingredient',
+    'image_manager'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+
+MEDIA_URL = '/media/'  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
